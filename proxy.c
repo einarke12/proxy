9fa101a66da4d0b7329e0f2898902e06a3817ba0
einarke12 <einar90karl@gmail.com>
2014-03-27 22:10:50 +0000

    Testing stuff

--- proxy.c
+++ proxy.c
@@ -16,14 +16,13 @@
 /*
  * Function prototypes
  */
-int parse_uri(char *uri, char *filename, char *cgiargs);
+int parse_uri(char *uri, char *hostname, char *pathname, int *port);
 void proxy(int fd);
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
 void read_requesthdrs(rio_t *rp);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
-
 void clienterror(int fd, char *cause, char *errnum,
             char *shortmsg, char *longmsg);
 
@@ -32,70 +31,46 @@ void clienterror(int fd, char *cause, char *errnum,
  */
 void prox(int fd) 
 {
-    int is_static;
+    int size 10;
+    int serverfd;
     struct stat sbuf;  
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char filename[MAXLINE], cgiargs[MAXLINE];
+    char filename[MAXLINE], pathname[MAXLINE], logstring[MAXLINE];
     rio_t rio;
 
     /* Read request line and headers */
     Rio_readinitb(&rio, fd);
     Rio_readlineb(&rio, buf, MAXLINE);
     sscanf(buf, "%s %s %s", method, uri, version);
+
+    format_log_entry(logstring, struct sockaddr_in *sockaddr, 
+              uri, int size)
+    
+    char parse_hostname[MAXLINE];
+    char parse_port[MAXLINE];
+    if (parse_uri(uri, parse_hostname, pathname, parse_port) == -1){ 
+        printf("Unable to parse the URI!\n");
+        return 0;
+    }
     
     if (strcasecmp(method, "GET")) {
         clienterror(fd, method, "501", "Not Implemented",
                     "Proxy does not implement this method");
         return;
     }
-    read_requesthdrs(&rio);
-
-    /* Parse URI from GET request */
-    is_static = parse_uri(uri, filename, cgiargs);
+    
+    if ( (serverfd = open_clientfd(parse_hostname, parse_port)) < 0){
+            printf("Unable to connect to web server! \n");
+            Close(serverfd);
+        return 0;
+    }
+    
     if (stat(filename, &sbuf) < 0) {
     clienterror(fd, filename, "404", "Not found",
                     "Proxy couldn’t find this file");
     return;
     }
 
-    if (is_static) { /* Serve static content */
-        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-            clienterror(fd, filename, "403", "Forbidden",
-                        "Proxy couldn’t read the file");
-        return;
-        }
-        serve_static(fd, filename, sbuf.st_size);
-    }
-    
-    else { /* Serve dynamic content */
-        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
-            clienterror(fd, filename, "403", "Forbidden",
-                        "Proxy couldn’t run the CGI program");
-            return;
-        }
-        serve_dynamic(fd, filename, cgiargs);
-    }
-}
-
-void clienterror(int fd, char *cause, char *errnum,
-                char *shortmsg, char *longmsg) {
-    char buf[MAXLINE], body[MAXBUF];
-
-    /* Build the HTTP response body */
-    sprintf(body, "<html><title>Tiny Error</title>");
-    sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
-    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
-    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
-
-    /* Print the HTTP response */
-    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
-    Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Content-type: text/html\r\n");
-    Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
-    Rio_writen(fd, buf, strlen(buf));
-    Rio_writen(fd, body, strlen(body));
 }
 
 void read_requesthdrs(rio_t *rp)
@@ -110,67 +85,13 @@ void read_requesthdrs(rio_t *rp)
     return;
 }
 
-void serve_static(int fd, char *filename, int filesize)
-{
-    int srcfd;
-    char *srcp, filetype[MAXLINE], buf[MAXBUF];
-
-    /* Send response headers to client */
-    get_filetype(filename, filetype);
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
-    sprintf(buf, "%sServer: Proxy Server\r\n", buf);
-    sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
-    sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
-    Rio_writen(fd, buf, strlen(buf));
-
-    /* Send response body to client */
-    srcfd = Open(filename, O_RDONLY, 0);
-    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-    Close(srcfd);
-    Rio_writen(fd, srcp, filesize);
-    Munmap(srcp, filesize);
-}
-
-/*
- * get_filetype - derive file type from file name
- */
-void get_filetype(char *filename, char *filetype)
-{
-    if (strstr(filename, ".html"))
-        strcpy(filetype, "text/html");
-    else if (strstr(filename, ".gif"))
-        strcpy(filetype, "image/gif");
-    else if (strstr(filename, ".jpg"))
-        strcpy(filetype, "image/jpeg");
-    else
-        strcpy(filetype, "text/plain");
-}
-
-void serve_dynamic(int fd, char *filename, char *cgiargs)
-{
-    char buf[MAXLINE], *emptylist[] = { NULL };
-
-    /* Return first part of HTTP response */
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
-    Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Server: Proxy  Server\r\n");
-    Rio_writen(fd, buf, strlen(buf));
-
-    if (Fork() == 0) { /* child */
-    /* Real server would set all CGI vars here */
-        setenv("QUERY_STRING", cgiargs, 1);
-        Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
-        Execve(filename, emptylist, environ); /* Run CGI program */
-    }
-    Wait(NULL); /* Parent waits for and reaps child */
-}
-
 /* 
  * main - Main routine for the proxy program 
  */
 int main(int argc, char **argv)
 {
     int listenfd, connfd, port, clientlen;
+    // Client port
     struct sockaddr_in clientaddr;
  
     /* Check command line args */
@@ -180,6 +101,7 @@ int main(int argc, char **argv)
     }
     port = atoi(argv[1]);
 
+    //Open and listen top argument port
     listenfd = Open_listenfd(port);
     while (1) {
         clientlen = sizeof(clientaddr);
@@ -198,35 +120,7 @@ int main(int argc, char **argv)
  * pathname must already be allocated and should be at least MAXLINE
  * bytes. Return -1 if there are any problems.
  */
-int parse_uri(char *uri, char *filename, char *cgiargs)
-{
-    char *ptr;
 
-    if (!strstr(uri, "cgi-bin")) { /* Static content */
-        strcpy(cgiargs, "");
-        strcpy(filename, ".");
-        strcat(filename, uri);
-        if (uri[strlen(uri)-1] == '/')
-            strcat(filename, "home.html");
-            return 1;
-        }
-    
-    else { /* Dynamic content */
-        ptr = index(uri, '?');
-
-        if (ptr) {
-            strcpy(cgiargs, ptr+1);
-            *ptr = '\0';
-        }
-        else
-            strcpy(cgiargs, "");
-            strcpy(filename, ".");
-            strcat(filename, uri);
-            return 0;
-    }
-}
-
-/*
 int parse_uri(char *uri, char *hostname, char *pathname, int *port)
 {
     char *hostbegin;
@@ -263,7 +157,7 @@ int parse_uri(char *uri, char *hostname, char *pathname, int *port)
     }
 
     return 0;
-}*/
+}
 
 /*
  * format_log_entry - Create a formatted log entry in logstring. 
